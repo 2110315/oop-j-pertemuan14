d577228d8074e6d288c52893a464a6acf9d6e677
2110315 <ichamude@gmail.com>
2025-06-17 22:06:07 +0800

    Add files via upload

---
+++ oop-j-pertemuan11/pertemuan11/pertemuan11/src/pertemuan11/Pertemuan11.java
@@ -0,0 +1,18 @@
+
+package pertemuan11;
+
+/**
+ *
+ * @author LAB F
+ * tgl 27 Mei 2025
+ */
+public class Pertemuan11 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
